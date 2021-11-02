cbb3f853e9a005da7a595d952579fe4ccba0a017
Jack <jmcgregor@coveo.com>
2021-11-02 15:50:27 -0400

    htmlSerializer types

--- lib/htmlSerializer.tsx
+++ lib/htmlSerializer.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Elements } from 'prismic-reactjs'
 
-export const htmlSerializer = (type, element, content, children, key) => {
+export const htmlSerializer = (type, _element, _content, children, _key) => {
     switch (type) {
         case Elements.heading1:
             return <>{children}</>
